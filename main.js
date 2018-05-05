56e079b1b289c3041ca0d53620f64d0c199b14a6
soga0901 <sg.0901.sg@gmail.com>
2018-05-05 18:19:44 +0900

    :tada: Initial Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const electron = require('electron')
+const url = require('url')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+  if (process.platform === null) createWindow()
+})
+
+function createWindow () {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname , 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }))
+
+  mainWindow.on('closed', function () {
+    mainWindow === null
+  })
+}
